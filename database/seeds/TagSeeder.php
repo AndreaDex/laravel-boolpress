815db7f5f3ec5c3aa60063f53ac63af69697a88f
AndreaDex <desiderio.andrea@outlook.it>
2021-07-28 15:59:34 +0200

    edit Tags table and seeder

--- database/seeds/TagSeeder.php
+++ database/seeds/TagSeeder.php
@@ -1,6 +1,8 @@
 <?php
 
+use App\Tag;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Str;
 
 class TagSeeder extends Seeder
 {
@@ -11,6 +13,13 @@ class TagSeeder extends Seeder
      */
     public function run()
     {
-        //
+        $tags = ['Art', 'Business', 'Comics', 'Design', 'Education', 'Family', 'Fitness', 'Food', 'Humor', 'Internet'];
+
+        foreach ($tags as $tag) {
+            $new = new Tag();
+            $new->name = $tag;
+            $new->slag = Str::slug($tag);
+            $new->save();
+        }
     }
 }
