d47fa8ed90784f6a0ff45e6c84026a648cbac50f
AndreaDex <desiderio.andrea@outlook.it>
2021-07-28 17:44:38 +0200

    add error messase boxes and old value for tags multiselects

--- resources/views/admin/posts/edit.blade.php
+++ resources/views/admin/posts/edit.blade.php
@@ -21,32 +21,47 @@
         <div class="form-group">
             <label for="title">Title</label>
             <input type="text" class="form-control" name="title" id="title" value="{{$post->title}}" placeholder="Inserisci il titolo del post">
-
         </div>
+        @error('title')
+        <div class="alert alert-danger">{{$message}}</div>
+        @enderror
+
         <!-- Sottotitolo -->
         <div class="form-group">
             <label for="subtitle">Sottotitolo</label>
             <input type="text" class="form-control" name="subtitle" id="subtitle" value="{{$post->subtitle}}" placeholder="Inserisci il sottotitolo del post">
-
         </div>
+        @error('subtitle')
+        <div class="alert alert-danger">{{$message}}</div>
+        @enderror
+
         <!-- Autore -->
         <div class="form-group">
             <label for="author">Autore</label>
             <input type="text" class="form-control" name="author" id="author" value="{{$post->author}}" placeholder="Inserisci il nome dell'autore">
-
         </div>
+        @error('author')
+        <div class="alert alert-danger">{{$message}}</div>
+        @enderror
+
         <!-- Contenuto -->
         <div class="form-group">
             <label for="body">Articolo</label>
             <textarea class="form-control" name="body" id="body" rows="3">{{$post->body}}</textarea>
         </div>
+        @error('body')
+        <div class="alert alert-danger">{{$message}}</div>
+        @enderror
+
         <!-- Poster -->
         <div class="form-group">
             <img src="{{asset('storage/'.$post->poster)}}" alt="">
             <label for="poster">Carica un'immagine</label>
             <input class="form-control" type="file" name="poster" id="poster">
-            <!-- <input type="text" class="form-control" name="poster" id="poster" value="{{$post->poster}}" placeholder="Inserisci il link ad una copertina"> -->
         </div>
+        @error('poster')
+        <div class="alert alert-danger">{{$message}}</div>
+        @enderror
 
         <!-- Categoria -->
         <div class="form-group">
@@ -58,6 +73,9 @@
                 @endforeach
             </select>
         </div>
+        @error('category_id')
+        <div class="alert alert-danger">{{$message}}</div>
+        @enderror
 
         <!-- Tags -->
         <div class="form-group">
@@ -67,12 +85,20 @@
                 @if ($tags)
 
                 @foreach ($tags as $tag )
+
+                @if ($errors->any())
+                <option value="{{$tag->id}}" {{in_array($tag->id, old('tags')) ? 'selected' : ''}}>{{$tag->name}}</option>
+                @else
                 <option value="{{$tag->id}}" {{$post->tags->contains($tag) ? 'selected' : ''}}>{{$tag->name}}</option>
+                @endif
                 @endforeach
 
                 @endif
             </select>
         </div>
+        @error('tags')
+        <div class="alert alert-danger">{{$message}}</div>
+        @enderror
 
         <!-- Submit -->
         <button type="submit" class="btn btn-primary">Submit</button>
